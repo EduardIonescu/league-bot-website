5e033931b611edc31eb889a2495db531fffc444f
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2025-02-23 23:39:40 +0100

    metadata + image

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import index from "./src/index.html";
 
-const server = Bun.serve({ port: 5173, routes: { "/": index } });
+const server = Bun.serve({
+  port: 5173,
+  routes: {
+    "/": index,
+    "/logo": async () =>
+      new Response(await Bun.file("./public/logo-front-shadow.webp").bytes()),
+  },
+});
 
 console.log(`Listening on: http://localhost:${server.port}`);
